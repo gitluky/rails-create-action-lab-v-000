4129caad835f3a5052ff61e5cacb64d66950962e
Jonathan Luk <jluk87@gmail.com>
2019-03-16 18:53:28 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,9 +9,15 @@ class StudentsController < ApplicationController
   end
 
   def new
+
   end
 
   def create
+    @post = Post.new
+    @post.first_name = params[:first_name]
+    @post.last_name = params[:last_name]
+    @post.save
+    redirect_to post_path(@post)
   end
 
 end
